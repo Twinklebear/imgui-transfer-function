df486e87afd45a3b0f161c04430ff3ebd80b883e
Will Usher <will@sci.utah.edu>
2019-09-10 16:42:28 -0600

    clang fmt

--- example/main.cpp
+++ example/main.cpp
@@ -1,15 +1,15 @@
+#include <array>
 #include <iostream>
 #include <memory>
 #include <vector>
-#include <array>
 #include <SDL.h>
 #include "gl_core_4_5.h"
 #include "imgui.h"
-#include "imgui_impl_sdl.h"
 #include "imgui_impl_opengl3.h"
+#include "imgui_impl_sdl.h"
 #include "shader.h"
-#include "transfer_function_widget.h"
 #include "stb_image.h"
+#include "transfer_function_widget.h"
 
 const std::string display_colormap_vs = R"(
 #version 450 core
@@ -50,147 +50,164 @@ int win_height = 720;
 
 void run_app(int argc, const char **argv, SDL_Window *window);
 
-int main(int argc, const char **argv) {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		std::cerr << "Failed to init SDL: " << SDL_GetError() << "\n";
-		return -1;
-	}
+int main(int argc, const char **argv)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "Failed to init SDL: " << SDL_GetError() << "\n";
+        return -1;
+    }
 
-	const char* glsl_version = "#version 450 core";
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+    const char *glsl_version = "#version 450 core";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
 
-	// Create window with graphics context
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    // Create window with graphics context
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
-	SDL_Window* window = SDL_CreateWindow("ImGui Transfer Function Demo",
-			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, win_width, win_height,
-			SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+    SDL_Window *window = SDL_CreateWindow("ImGui Transfer Function Demo",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          win_width,
+                                          win_height,
+                                          SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 
-	SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-	SDL_GL_SetSwapInterval(1);
-	SDL_GL_MakeCurrent(window, gl_context);
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_SetSwapInterval(1);
+    SDL_GL_MakeCurrent(window, gl_context);
 
-	if (ogl_LoadFunctions() == ogl_LOAD_FAILED) {
-		std::cerr << "Failed to initialize OpenGL\n";
-		return 1;
-	}
+    if (ogl_LoadFunctions() == ogl_LOAD_FAILED) {
+        std::cerr << "Failed to initialize OpenGL\n";
+        return 1;
+    }
 
-	// Setup Dear ImGui context
-	ImGui::CreateContext();
+    // Setup Dear ImGui context
+    ImGui::CreateContext();
 
-	// Setup Dear ImGui style
-	ImGui::StyleColorsDark();
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
 
-	// Setup Platform/Renderer bindings
-	ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
-	ImGui_ImplOpenGL3_Init(glsl_version);
+    // Setup Platform/Renderer bindings
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init(glsl_version);
 
-	run_app(argc, argv, window);
+    run_app(argc, argv, window);
 
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplSDL2_Shutdown();
-	ImGui::DestroyContext();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
 
-	SDL_GL_DeleteContext(gl_context);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
-	return 0;
+    return 0;
 }
 
-void run_app(int argc, const char **argv, SDL_Window *window) {
-	ImGuiIO& io = ImGui::GetIO();
+void run_app(int argc, const char **argv, SDL_Window *window)
+{
+    ImGuiIO &io = ImGui::GetIO();
 
-	TransferFunctionWidget tfn_widget;
+    TransferFunctionWidget tfn_widget;
 
-	// Load any extra colormaps the user wants to see in the demo
-	for (int i = 1; i < argc; ++i) {
-		int w, h, n;
-		uint8_t *img_data = stbi_load(argv[i], &w, &h, &n, 4);
-		auto img = std::vector<uint8_t>(img_data, img_data + w * 1 * 4);
-		stbi_image_free(img_data);
-		tfn_widget.add_colormap(Colormap(argv[i], img));
-	}
+    // Load any extra colormaps the user wants to see in the demo
+    for (int i = 1; i < argc; ++i) {
+        int w, h, n;
+        uint8_t *img_data = stbi_load(argv[i], &w, &h, &n, 4);
+        auto img = std::vector<uint8_t>(img_data, img_data + w * 1 * 4);
+        stbi_image_free(img_data);
+        tfn_widget.add_colormap(Colormap(argv[i], img));
+    }
 
-	// A texture so we can color the background of the window by the colormap
-	GLuint colormap_texture;
-	glGenTextures(1, &colormap_texture);
-	glBindTexture(GL_TEXTURE_1D, colormap_texture);
-	glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	auto colormap = tfn_widget.get_colormap();
-	glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA8, colormap.size() / 4, 0,
-			GL_RGBA, GL_UNSIGNED_BYTE, colormap.data());
+    // A texture so we can color the background of the window by the colormap
+    GLuint colormap_texture;
+    glGenTextures(1, &colormap_texture);
+    glBindTexture(GL_TEXTURE_1D, colormap_texture);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    auto colormap = tfn_widget.get_colormap();
+    glTexImage1D(GL_TEXTURE_1D,
+                 0,
+                 GL_RGBA8,
+                 colormap.size() / 4,
+                 0,
+                 GL_RGBA,
+                 GL_UNSIGNED_BYTE,
+                 colormap.data());
 
-	GLuint vao;
-	glCreateVertexArrays(1, &vao);
-	glBindVertexArray(vao);
+    GLuint vao;
+    glCreateVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glDisable(GL_DEPTH_TEST);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glDisable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
 
-	Shader display_colormap(display_colormap_vs, display_colormap_fs);
+    Shader display_colormap(display_colormap_vs, display_colormap_fs);
 
-	bool done = false;
-	while (!done) {
-		SDL_Event event;
-		while (SDL_PollEvent(&event)) {
-			ImGui_ImplSDL2_ProcessEvent(&event);
-			if (event.type == SDL_QUIT) {
-				done = true;
-			}
-			if (!io.WantCaptureKeyboard && event.type == SDL_KEYDOWN
-					&& event.key.keysym.sym == SDLK_ESCAPE)
-			{
-				done = true;
-			}
-			if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE
-					&& event.window.windowID == SDL_GetWindowID(window)) {
-				done = true;
-			}
-			if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED) {
-				win_width = event.window.data1;
-				win_height = event.window.data2;
-				io.DisplaySize.x = win_width;
-				io.DisplaySize.y = win_height;
-			}
-		}
+    bool done = false;
+    while (!done) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT) {
+                done = true;
+            }
+            if (!io.WantCaptureKeyboard && event.type == SDL_KEYDOWN &&
+                event.key.keysym.sym == SDLK_ESCAPE) {
+                done = true;
+            }
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE &&
+                event.window.windowID == SDL_GetWindowID(window)) {
+                done = true;
+            }
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED) {
+                win_width = event.window.data1;
+                win_height = event.window.data2;
+                io.DisplaySize.x = win_width;
+                io.DisplaySize.y = win_height;
+            }
+        }
 
-		if (tfn_widget.changed()) {
-			auto colormap = tfn_widget.get_colormap();
-			glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA8, colormap.size() / 4, 0,
-					GL_RGBA, GL_UNSIGNED_BYTE, colormap.data());
-		}
+        if (tfn_widget.changed()) {
+            auto colormap = tfn_widget.get_colormap();
+            glTexImage1D(GL_TEXTURE_1D,
+                         0,
+                         GL_RGBA8,
+                         colormap.size() / 4,
+                         0,
+                         GL_RGBA,
+                         GL_UNSIGNED_BYTE,
+                         colormap.data());
+        }
 
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplSDL2_NewFrame(window);
-		ImGui::NewFrame();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame(window);
+        ImGui::NewFrame();
 
-		ImGui::Begin("Debug Panel");
-		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-				1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		tfn_widget.draw_ui();
-		ImGui::End();
+        ImGui::Begin("Debug Panel");
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                    1000.0f / ImGui::GetIO().Framerate,
+                    ImGui::GetIO().Framerate);
+        tfn_widget.draw_ui();
+        ImGui::End();
 
-		// Rendering
-		ImGui::Render();
-		glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+        // Rendering
+        ImGui::Render();
+        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
 
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glUseProgram(display_colormap.program);
-		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(display_colormap.program);
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		SDL_GL_SwapWindow(window);
-	}
+        SDL_GL_SwapWindow(window);
+    }
 }
 
--- transfer_function_widget.h
+++ transfer_function_widget.h
@@ -1,65 +1,67 @@
 #pragma once
 
-#include <string>
 #include <cstdint>
+#include <string>
 #include <vector>
+#include "gl_core_4_5.h"
 #include "imgui.h"
 
-#include "gl_core_4_5.h"
-
 struct Colormap {
-	std::string name;
-	// An RGBA8 1D image
-	std::vector<uint8_t> colormap;
+    std::string name;
+    // An RGBA8 1D image
+    std::vector<uint8_t> colormap;
 
-	Colormap(const std::string &name, const std::vector<uint8_t> &img);
+    Colormap(const std::string &name, const std::vector<uint8_t> &img);
 };
 
 class TransferFunctionWidget {
-	struct vec2f {
-		float x, y;
+    struct vec2f {
+        float x, y;
 
-		vec2f(float c = 0.f);
-		vec2f(float x, float y);
-		vec2f(const ImVec2 &v);
+        vec2f(float c = 0.f);
+        vec2f(float x, float y);
+        vec2f(const ImVec2 &v);
 
-		float length() const;
+        float length() const;
 
-		vec2f operator+(const vec2f &b) const;
-		vec2f operator-(const vec2f &b) const;
-		vec2f operator/(const vec2f &b) const;
-		vec2f operator*(const vec2f &b) const;
-		operator ImVec2() const;
-	};
+        vec2f operator+(const vec2f &b) const;
+        vec2f operator-(const vec2f &b) const;
+        vec2f operator/(const vec2f &b) const;
+        vec2f operator*(const vec2f &b) const;
+        operator ImVec2() const;
+    };
 
-	std::vector<Colormap> colormaps;
-	size_t selected_colormap = 0;
-	std::vector<uint8_t> current_colormap;
+    std::vector<Colormap> colormaps;
+    size_t selected_colormap = 0;
+    std::vector<uint8_t> current_colormap;
 
-	std::vector<vec2f> alpha_control_pts = {vec2f(0.f), vec2f(1.f)};
-	size_t selected_point = -1;
+    std::vector<vec2f> alpha_control_pts = {vec2f(0.f), vec2f(1.f)};
+    size_t selected_point = -1;
 
-	bool colormap_changed = true;
-	GLuint colormap_img = -1;
+    bool colormap_changed = true;
+    GLuint colormap_img = -1;
 
 public:
-	TransferFunctionWidget();
+    TransferFunctionWidget();
 
-	// Add a colormap preset. The image should be a 1D RGBA8 image
-	void add_colormap(const Colormap &map);
-	// Add the transfer function UI into the currently active window
-	void draw_ui();
-	// Returns true if the colormap was updated since the last
-	// call to draw_ui
-	bool changed() const;
-	// Get back the RGBA8 color data for the transfer function
-	std::vector<uint8_t> get_colormap();
+    // Add a colormap preset. The image should be a 1D RGBA8 image
+    void add_colormap(const Colormap &map);
+
+    // Add the transfer function UI into the currently active window
+    void draw_ui();
+
+    // Returns true if the colormap was updated since the last
+    // call to draw_ui
+    bool changed() const;
+
+    // Get back the RGBA8 color data for the transfer function
+    std::vector<uint8_t> get_colormap();
 
 private:
-	void update_gpu_image();
-	void update_colormap();
+    void update_gpu_image();
 
-	void load_embedded_preset(const uint8_t *buf, size_t size,
-			const std::string &name);
+    void update_colormap();
+
+    void load_embedded_preset(const uint8_t *buf, size_t size, const std::string &name);
 };
 
